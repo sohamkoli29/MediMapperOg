222362dce77c32841e8d28d8a4b5608a4f3ba9a2
Soham Suraj Koli <143392783+sohamkoli29@users.noreply.github.com>
2025-03-20 00:48:52 +0530

    Update config.php

--- scripts/config.php
+++ scripts/config.php
@@ -10,5 +10,5 @@ $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-define('API_KEY', ''); 
+define('API_KEY', 'YOUR_API_KEY'); 
 ?>
